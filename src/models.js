4906dc6c2c8dde4589a2c73cfe4825df84dcc8b8
EmersonBerido <beridoemerson05@gmail.com>
2025-10-18 15:01:25 -0700

    Added Logo + changed prompt

--- src/models.js
+++ src/models.js
@@ -1,4 +1,4 @@
-class Peer {
+export class Peer {
   constructor(name, descriptors) {
     this.name = name;
     this.descriptors = descriptors;
--- src/prompt.js
+++ src/prompt.js
@@ -1,18 +1,19 @@
-function constructPrompt(reqBody) {
+import {Peer} from "./models.js"
+export function constructPrompt(reqBody) {
   //dummy data
   const peersList = [];
 
-  const peer1 = new Peer("John Doe", ["Python", "AI/LLM", "Data Scientist"]);
+  const peer1 = new Peer("John Doe", ["HTML", "REST", "Database Integration"]);
   peersList.push(peer1);
 
   const peer2 = new Peer("Jane Smith", [
-    "JavaScript",
-    "Prompt engineer",
-    "MongoDB",
+    "Express",
+    "UI/UX",
+    "Tailwind",
   ]);
   peersList.push(peer2);
 
-  const peer3 = new Peer("Another Person", ["Engineer", "AI/LLM", "Hackathon"]);
+  const peer3 = new Peer("Another Person", ["Authentification", "REST", "React"]);
   peersList.push(peer3);
 
   //concat to create prompt string
