34bae455902cb6e160c563b8bf08ee0351b389eb
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-10-18 14:34:54 -0700

    Format files

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npx prettier --write .
-node src/main.js
+npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 //Make sure they can only have three max
 
@@ -7,131 +7,125 @@ function App() {
   const [displayPrompt, setDisplayPrompt] = useState(true);
   const [responses, setResponses] = useState(<p>No Responses So Far</p>);
 
-  function handleSubmit(event)
-  {
+  function handleSubmit(event) {
     event.preventDefault();
 
     const name = event.target.name.value;
-    const descriptors = Array.from(event.target.querySelectorAll('input[name="descriptors"]:checked'))
-    .map(input => input.value);
+    const descriptors = Array.from(
+      event.target.querySelectorAll('input[name="descriptors"]:checked'),
+    ).map((input) => input.value);
 
-    // Need atleast one field 
-    if (!name && descriptors.length === 0)
-    {
+    // Need atleast one field
+    if (!name && descriptors.length === 0) {
       alert("Empty form");
       return;
     }
 
     // Cant have more than 3
-    if (descriptors.length > 3)
-    {
+    if (descriptors.length > 3) {
       alert("Cant have more than 3");
       return;
     }
 
     const reqBody = JSON.stringify({
-      name : name,
-      descriptors : descriptors
-    })
+      name: name,
+      descriptors: descriptors,
+    });
     // console.log(reqBody)
 
     // Send to backend
     fetch("httpBLABLABLA", {
-      headers : {
-        "Content-Type" : "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
-      body : reqBody
-    })
-      .then(response => console.log(response));
+      body: reqBody,
+    }).then((response) => console.log(response));
 
     //returns a JSON
-
-
-
 
     // Display people
     setDisplayPrompt(false);
   }
 
-
-
-
   return (
     <main>
-
-      { displayPrompt ?
+      {displayPrompt ? (
         <form onSubmit={handleSubmit}>
           <h1>Team Search</h1>
 
-          <div className='name-container'>
-            <input type='text' name="name" id="name" placeholder='Enter name...'/>
+          <div className="name-container">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter name..."
+            />
           </div>
 
-          <small className='descriptors-name'><u>Skills & Technologies</u></small>
-          <div className='descriptors-container'>
-
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Authentification" />
+          <small className="descriptors-name">
+            <u>Skills & Technologies</u>
+          </small>
+          <div className="descriptors-container">
+            <div className="type-value">
+              <input
+                type="checkbox"
+                name="descriptors"
+                value="Authentification"
+              />
               Authentification
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Express"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="Express" />
               REST
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Database"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="Database" />
               Database Integration
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Animation"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="Animation" />
               Animation
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="UI/UX"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="UI/UX" />
               UI/UX
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Express"/>
-              Express   
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="Express" />
+              Express
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="HTML"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="HTML" />
               HTML
             </div>
 
-
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="React"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="React" />
               React
             </div>
 
-            <div className='type-value'>
-              <input type="checkbox" name="descriptors" value="Tailwind"/>
+            <div className="type-value">
+              <input type="checkbox" name="descriptors" value="Tailwind" />
               Tailwind
             </div>
-
           </div>
 
           <button type="submit"> Look For Teammates </button>
-
-
         </form>
-
-        :
+      ) : (
         <h1>
           {responses}
           <button onClick={() => setDisplayPrompt(true)}>Search</button>
         </h1>
-      }
-      
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
